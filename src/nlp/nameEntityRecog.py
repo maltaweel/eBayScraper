d206ed87b7c7fb7d816adfa1830003fb79ed437e
Mark Altaweel <maltaweel@ucl.ac.uk>
2019-01-16 17:33:28 +0000

    commit

--- src/nlp/nameEntityRecog.py
+++ src/nlp/nameEntityRecog.py
@@ -6,12 +6,13 @@ Created on Jan 16, 2019
 
 import spacy
 import csv
-from spacy import displacy
-from collections import Counter
-import en_core_web_sm
 import nltk
 import os
-nlp = en_core_web_sm.load()
+import random
+
+spacy.prefer_gpu()
+nlp = spacy.load('en')
+
 
 def preprocess(sent):
     sent = nltk.word_tokenize(sent)
@@ -81,10 +82,38 @@ def lookAtText(results):
         doc = nlp(u)
     
         print([(X.text, X.label_) for X in doc.ents])
+
+def printResults(results):
     
+    fieldnames = ['Object','Price','Location','Links']
+     
+    pn=os.path.abspath(__file__)
+    pn=pn.split("src")[0]
+    fileOutput=os.path.join(pn,'output',"namedEntity.csv")
+    
+    with open(fileOutput, 'wb') as csvf:
+        writer = csv.DictWriter(csvf, fieldnames=fieldnames)
+
+        writer.writeheader()      
+    
+        for d in results:
+            obj=results[d]
+            
+            res1=""
+            res2=""
+            res3=""
+            res4=""
+            
+            writer.writerow({'Object':str(res1),'Price':str(res2),'Location':str(res3),'Links':str(res4)})
+    
+
+    
+def run():
+#    train_model()
+    results=loadData()
+    lookAtText(results)
+   
+    print("Finished")
+   
 if __name__ == '__main__':
-   results=loadData()
-   
-   lookAtText(results)
-   
-   print("Finished")
+    run()
